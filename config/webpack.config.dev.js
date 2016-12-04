1c175dda8d573dcc5d90a85a1c9ba7db37030195
Pedro S. Moreira <pedsmoreira@gmail.com>
2016-12-04 10:57:50 -0200

    Add Sass loader, animates, axios, soundplayer (soundcloud) and react-router-bootstrap packages

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -121,8 +121,8 @@ module.exports = {
       // In production, we use a plugin to extract that CSS to a file, but
       // in development "style" loader enables hot editing of CSS.
       {
-        test: /\.css$/,
-        loader: 'style!css?importLoaders=1!postcss'
+        test: /\.(css|scss|sass)$/,
+        loader: 'style!css?importLoaders=2!sass!postcss'
       },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
